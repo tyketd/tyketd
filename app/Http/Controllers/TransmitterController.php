9664a46fb13de498d4a7bfe82b0f28e92a27cfdf
yankyKEZ <yarnoghislain@yahoo.fr>
2018-05-05 20:23:42 +0200

    creation des utilisateurs transmitter,forwardingArgent, support,Administrator

---
+++ app/Http/Controllers/TransmitterController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transmitter;
+use Illuminate\Http\Request;
+
+class TransmitterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Transmitter  $transmitter
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transmitter $transmitter)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Transmitter  $transmitter
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Transmitter $transmitter)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Transmitter  $transmitter
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Transmitter $transmitter)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Transmitter  $transmitter
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transmitter $transmitter)
+    {
+        //
+    }
+}

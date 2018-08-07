9b9cf8c21064eea5e12e947c075beaef96753c27
Stratege <213346@supinfo.com>
2018-08-07 12:34:45 +0200

    Petite exemple de classe avec des attributs fictifs et quelques fonctions

--- resources/assets/Welcome/js/other.js
+++ resources/assets/Welcome/js/other.js
@@ -1,16 +1,50 @@
 $(function () {
-    //bootbox.alert("i am a test", ()=>{});
-
-    class THWelcome {
+    class WelcomeClass {
         constructor(){
             this.Id = $("#Welcome");
+            this.Block ={
+                Item1 : $("#block .iteml"),
+                Item3 : $("#block .item2"),
+                Item2 : $("#block .item3"),
+                MyAlert: function (message) {
+                    bootbox.alert(message, ()=>{
+                        console.log("end action");
+                    });
+                }
+            };
+            this.Confirm = function (message) {
+                bootbox.confirm(message, (result)=>{
+                    if(result){
+                        console.log("Ok .....");
+                    }
+                    else {
+                        console.log("No ......");
+                    }
+                });
+            };
+            this.Promp = function (message) {
+                bootbox.confirm(message, (result)=>{
+                    if(result){
+                        console.log("validate .....");
+                    }
+                });
+            }
         }
     }
 
-    let welcomClass = new THWelcome();
+    let MyWelcomClass = new WelcomeClass();
 
-    if(welcomClass.dat("page")==="Welcome"){
+    if(MyWelcomClass.data("page")==="Welcome"){
         //do  something  here
+
+        // alert
+        //MyWelcomClass.Block.MyAlert("Simple alert");
+
+        //Promp
+        //MyWelcomClass.Promp("Entrer something");
+
+        //confirm
+        //MyWelcomClass.Confirm("Are you sure to do this ?");
     }
 
 });
